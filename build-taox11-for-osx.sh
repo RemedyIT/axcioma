a37f5c020dc5c7d00bccf9b417dacbc39a6bedd5
ClausKlein <claus.klein@arcormail.de>
2022-04-18 14:45:34 +0200

    add patch to build ACE with -std=c++17

--- build-taox11-for-osx.sh
+++ build-taox11-for-osx.sh
@@ -28,6 +28,12 @@ rm -f *.log
 
 $X11_BASE_ROOT/bin/brix11 bootstrap taox11
 
+############################################################
+# patch to build ACE with -std=c++17
+cd $ACE_ROOT && patch -p1 < ../ACE_Auto_Ptr.patch
+cd $X11_BASE_ROOT
+############################################################
+
 $X11_BASE_ROOT/bin/brix11 configure -W aceroot=$ACE_ROOT -W taoroot=$TAO_ROOT -W mpcroot=$MPC_ROOT
 
 # Print brix11 configuration
@@ -51,6 +57,15 @@ echo ${platform_file} > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
 
 # ACE/ACE/ace/config.h
 # ACE/ACE/ace/config-macosx.h
+# ACE/ACE/ace/config-macosx-mojave.h
+# ACE/ACE/ace/config-macosx-highsierra.h
+# ACE/ACE/ace/config-macosx-sierra.h
+# ACE/ACE/ace/config-macosx-elcapitan.h
+# ACE/ACE/ace/config-macosx-yosemite.h
+# ACE/ACE/ace/config-macosx-mavericks.h
+# ACE/ACE/ace/config-macosx-mountainlion.h
+# ACE/ACE/ace/config-macosx-lion.h
+# ACE/ACE/ace/config-macosx-leopard.h
 echo '#include "ace/config-macosx.h"' > ${ACE_ROOT}/ace/config.h
 
 # ACE/ACE/bin/MakeProjectCreator/config/default.features
@@ -59,9 +74,8 @@ echo 'versioned_namespace=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/defaul
 echo 'acetaompc=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
 echo 'inline=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
 echo 'optimize=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
-# TODO: echo 'c++17=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
 
-# generate all GNUmakefile
+# generate all GNUmakefile's
 # see workspace.mwc
 perl ${TAOX11_ROOT}/bin/mwc.pl -type gnuace ${X11_BASE_ROOT}/workspace.mwc -workers ${BRIX11_NUMBER_OF_PROCESSORS}
 perl ${TAOX11_ROOT}/bin/mwc.pl -type gnuace ${TAOX11_ROOT}/orbsvcs/tests -workers ${BRIX11_NUMBER_OF_PROCESSORS}
@@ -69,10 +83,10 @@ perl ${TAOX11_ROOT}/bin/mwc.pl -type gnuace ${TAOX11_ROOT}/examples -workers ${B
 perl ${TAOX11_ROOT}/bin/mwc.pl -type gnuace ${TAOX11_ROOT}/tests -workers ${BRIX11_NUMBER_OF_PROCESSORS}
 
 # make all
-make -j ${BRIX11_NUMBER_OF_PROCESSORS} 2>&1 | tee make-all.log
-make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/orbsvcs/tests 2>&1 | tee -a make-all.log
-make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/examples 2>&1 | tee -a make-all.log
-make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/tests 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} 2>&1 | tee make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/orbsvcs/tests 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/examples 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C ${TAOX11_ROOT}/tests 2>&1 | tee -a make-all.log
 
 # make tests
 $X11_BASE_ROOT/bin/brix11 run list -l taox11/bin/taox11_tests.lst -r taox11 2>&1 | tee run-list.log
